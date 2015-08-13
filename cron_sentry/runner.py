cf149549d933f418c7ef47cd652d2a103f92e8cc
Hugo Lopes Tavares <hltbra@gmail.com>
2015-08-13 13:52:45 -0400

    Add parameter `max-list-length` to command line. Refs #9

--- cron_sentry/runner.py
+++ cron_sentry/runner.py
@@ -30,6 +30,11 @@ parser.add_argument(
     help='The maximum characters of a string that should be sent to Sentry',
 )
 parser.add_argument(
+    '--max-list-length',
+    type=int,
+    help='The maximum number of items in a list-like object that should be sent to Sentry',
+)
+parser.add_argument(
     '--version',
     action='version',
     version=VERSION,
@@ -79,7 +84,12 @@ def run(args=argv[1:]):
             cmd = opts.cmd[1:]
         else:
             cmd = opts.cmd
-        runner = CommandReporter(cmd=cmd, dsn=opts.dsn, string_max_length=opts.string_max_length)
+        runner = CommandReporter(
+            cmd=cmd,
+            dsn=opts.dsn,
+            string_max_length=opts.string_max_length,
+            max_list_length=opts.max_list_length,
+        )
         sys.exit(runner.run())
     else:
         sys.stderr.write("ERROR: Missing command parameter!\n")
@@ -88,10 +98,11 @@ def run(args=argv[1:]):
 
 
 class CommandReporter(object):
-    def __init__(self, cmd, dsn, string_max_length=None):
+    def __init__(self, cmd, dsn, string_max_length=None, max_list_length=None):
         self.dsn = dsn
         self.command = cmd
         self.string_max_length = string_max_length
+        self.max_list_length = max_list_length
 
     def run(self):
         start = time()
@@ -117,7 +128,11 @@ class CommandReporter(object):
 
         message = "Command \"%s\" failed" % (self.command,)
 
-        client = Client(dsn=self.dsn, string_max_length=self.string_max_length)
+        client = Client(
+            dsn=self.dsn,
+            string_max_length=self.string_max_length,
+            max_list_length=self.max_list_length,
+        )
 
         client.captureMessage(
             message,
